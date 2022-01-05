0c992e621e1ba19737ac009a16772e6794dc8f8c
Kerry Kreiter <43457152+comm-scripter@users.noreply.github.com>
2022-01-05 15:04:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -131,8 +131,90 @@ let viewSelection = document.getElementById("show-selection");
 viewSelection.innerHTML = "";
 //prevBtn.innerHTML = "";//clears day from button
 //nextBtn.innerHTML = "";
+let dailyScript;
 let scriptView = document.getElementById("scripture");
 let viewInstruct = document.getElementById("show-instructions");
+let quoteMatrix = [{Evil: [{sun: 'QUOTE:\n\n"God would rather transform the EVIL man than eradicate him."\n(Billy Graham)'}, 
+{mon: 'QUOTE:\n\n"Satan is EVIL in himself."\n(Peter Nii Korley)'},
+ {tue: 'QUOTE:\n\n"It is in the close similarity between good and EVIL, right and wrong, that the danger lies."\n(Billy Graham)'}, 
+{wed: 'QUOTE:\n\n"If you want to chart the course of your life and live a fulfilling life, restrain and refrain your tongue from EVIL."\n(Justice Kojo Bentil)'},
+ {thu: 'QUOTE:\n\n"The entry of light forces the exit of the darkness (EVIL)."\n(Mike Murdock)'}, 
+{fri: 'QUOTE:\n\n"EVIL is what disagrees with the provision God has made concerning a matter"\n(Justice Kojo Bentil)'},
+ {sat: 'QUOTE:\n\n"This scripture says the love of money is the very root of Satan\'s operation. We can cut him off at the very root and destroy his base with the love of giving. Glory to God."\n(Kenneth M. Copeland)'},
+ {bonus: 'QUOTE:\n\n"Anything good is hated by everything EVIL."\n(Mike Murdock)'}]},
+{Forgiveness: [{sun: 'QUOTE:\n\n"To err is human, to forgive divine"\n\n(Alexander Pope)'}, 
+{mon: 'QUOTE:\n\n"Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love."\n\n(Martin Luther King, Jr.)'},
+ {tue: 'QUOTE:\n\n"Holding a grudge does not make you strong; it makes you bitter. Forgiving does not make you weak, it sets you free."\n\n(Dave Willis)'}, 
+{wed: 'QUOTE:\n\n"There is a nobility in compassion, a beauty in empathy, a grace in forgiveness."\n\n(John Connolly)'},
+ {thu: 'QUOTE:\n\n"Forgiveness is a funny thing. It warms the heart and cools the sting."\n\n(William Arthur Ward)'}, 
+{fri: 'QUOTE:\n\n"The weak can never forgive. Forgiveness is the attribute of the strong."\n\n(Mahatmas Gandi)'},
+ {sat: 'QUOTE:\n\n"A broken friendship that is mended through forgiveness can be even stronger than it ever was."\n\n(Stephan Richards)'}]},
+ {Happiness: [{sun: '1 Chronicles 16:10\n"Glory in His holy name, let the heart of those who seek the Lord be glad."'}, 
+ {mon: 'Proverbs 16:20\n"Blessed (happy) is he who trusts in the Lord."'},
+  {tue: 'Romans 15:13\n"Now may the God of hope fill you with all joy and peace."'}, 
+ {wed: 'Proverbs 14:21\n"Happy is he who is gracious to the poor."'},
+  {thu: 'Psalm 132:9\n"Let your Godly people sing for joy."'}, 
+ {fri: 'Psalm 144:5\n"How blessed (happy) are the people whose God is the Lord."'},
+  {sat: 'Proverbs 29:18\n"Happy is he who keeps the law (God\'s Commandments)."'}]},
+  {Noah_1: [{sun: "Genesis 1:31\nThen God looked over all He had made, and He saw that it was very good."}, 
+  {mon: "Romans 6:13\nSurrender your whole being to God to be used for righteous purpose."},
+   {tue: "Isaiah 30:15\nIn repentance and rest is your salvation, in quietness and trust is your strength."}, 
+  {wed: 'John 7:37\nJesus stood and cried out, "If anyone thirsts, let him come to Me and drink." (living water)'},
+   {thu: "Proverb 23:17\nDo not let your heart envy sinners, but live in fear of the Lord."}, 
+  {fri: "Proverb 21:23\nHe (the person) who guards his/her tongue guards their soul from trouble."},
+   {sat: "1 John 4:16\nAnd so we know and rely on the love God has for us. God is love."}]},
+   {Noah_2: [{sun: "Psalm 2:11\nWorship the Lord with reverence and rejoice with trembling."}, 
+   {mon: "Psalm 66:4\nAll the earth will worship You (God), and will sing praises to You."},
+    {tue: "Isaiah 29:13\nPeople honor Me with their lips but their heart is far away from Me."}, 
+   {wed: "John 4:24\nGod is spirit and those who worship Him must worship in spirit and truth."},
+    {thu: "Exodus 34:14\nFor you shall not worship any other god, for the Lord, whose name is Jealous is a jealous God."}, 
+   {fri: 'Matthew 4:10\nJesus said,"Begone Satan, for it is written: you shall worship the Lord your God, and serve Him only."'},
+    {sat: "Revelation 4:11\nWorthy are You our Lord and our God, to receive glory and honor and power for You created all things."}]},
+    {Pray: [{sun: 'Matthew 21:21-22\n"And Jesus answered and said to them, \'If you have faith, and do not doubt...everything you ask in prayer, believing, you shall receive.\'"'}, 
+    {mon: 'Colossians 4:2\n"Devote yourself to prayer, keeping alert in it with an attitude of thanksgiving."'},
+     {tue: 'Matthew 6:6\n"When you pray go into your inner room...shut your door, pray to your Father who is in secret, and your Father...will repay you."'}, 
+    {wed: 'Matthew 5:44\n"But I say to you, \'Love your enemies and pray for those who persecute you.\'"'},
+     {thu: 'Psalm 4:1\n"Answer me when I call O God of my righteousness, and God\'s ears attend to my prayer."'}, 
+    {fri: '1 Thessalonians 5:17\n"Pray without ceasing."'},
+     {sat: '1 Samuel 2:1\n"Then Hannah prayed and said, \'My heart exalts in the Lord...because I rejoice in Your salvation.\'"'}]},
+     {Spirit: [{sun: 'Titus 3:6\n"For God abundantly poured out the Holy Spirit on us through Jesus Christ our Savior."'}, 
+     {mon: '2 Timothy 1:14\n"Keep the good things that have been entrusted to you, through the power of the Holy Spirit who lives in us."'},
+      {tue: 'Mark 1:18\n"I, John the Baptist baptize you with water, but Jesus will baptize you with the Holy Spirit."'}, 
+     {wed: '1 John 4:2\n"This is how you will know it is God\'s Spirit: everyone who declares that Jesus Christ became mortal man has the Spirit Who comes from God."'},
+      {thu: '1 John 4:13\n"This is how we are sure that we live in God and He lives in us: He has given us His Spirit."'}, 
+     {fri: 'Ephesians 4:30\n"Do not grieve God\'s Holy Spirit, for the Spirit is God\'s ownership on you, a guarantee that the day will come when God sets you free."'},
+      {sat: 'Galatians 5:25\n"The Spirit has given us life: He must also control our lives."'}]},
+      {Study: [{sun: '1 Corinthians 1:18\n"For the Word of the cross is to those who are perishing foolishness; but to us who are being saved it is the power of God."'}, 
+      {mon: '2 Timothy 3:16\n"All Scripture is inspired by God and profitable for teaching, reproof, correction, for training in righteousness that the man of God may be adequately equipped for every good work."'},
+       {tue: '2 Peter 1:20-21\n"But know this first of all, that no prophesy of Scripture is a matter of one\'s own interpretation, for no prophesy was ever made by an act of human will, but men moved by the Holy Spirit spoke from God."'}, 
+      {wed: 'Psalm 119:11\n"Your Word, Lord, I have treasured in my heart, that I may not sin against you."'},
+       {thu: 'Isaiah 55:11\n"So shall My Word be which goes from my mouth; it shall not return to Me empty."'}, 
+      {fri: 'Matthew 4:4\n"But He (Jesus) answered and said, \'Man shall not live on bread alone, but on every word that proceeds out of the mouth of God.\'"'},
+       {sat: 'Matthew 25:13\n"Watch therefore, for you know neither the day nor the hour in which the Son of Man is coming."'}]},
+       {Sword: [{sun: 'Ephesians 6:18\n"With all prayer and petition, PRAY at all times in the SPIRIT, with this in view, be on the alert with all perseverance and petition for all the saints."'}, 
+       {mon: 'Ephesians 6:11\n"Put on the full ARMOR of God, that you may be able to stand firm against the schemes of the devil."'},
+        {tue: 'Ephesians 6:14\n"Having girded your loins with TRUTH."'}, 
+       {wed: 'Ephesians 6:14\n"Having put on the breastplate of RIGHTEOUSNESS."'},
+        {thu: 'Ephesians 6:15\n"Having shod your feet with the preparation of the good news of PEACE."'}, 
+       {fri: 'Ephesians 6:16\n"In addition to all, taking up the SHIELD OF FAITH."'},
+        {sat: 'Ephesians 6:17\n"And take the HELMET OF SALVATION and the SWORD OF THE SPIRIT, which is the WORD OF GOD."'}]},
+        {Thankful: [{sun: 'Psalm 105:1,3\n"O give thanks to the Lord, call upon His name. Make known His deeds among the people. Let the heart of those who seek the Lord be glad."'}, 
+        {mon: 'Psalm 9:1\n"I will give thanks to the Lord with all my heart. I will tell of all Your wonders. I will be glad and exalt in You. I will sing praise to Your name, O Most High."'},
+         {tue: 'Psalm 7:17\n"I will give to the Lord according to His righteousness and will sing praise to the name of the Lord Most High."'}, 
+        {wed: 'Romans 6:17\n"But thanks be to God that though you were slaves to sin, you became obedient from the heart to that form of teaching to which you were committed."'},
+         {thu: 'Ephesians 5:20\n"Always giving thanks for all things in the name of our Lord Jesus Christ to God even the Father."'}, 
+        {fri: '1 Chronicles 16:34\n"O give thanks to the Lord for He is good; for His lovingkindness is everlasting."'},
+         {sat: 'Psalm 95:2-3\n"Let us come before the Lord\'s presence with thanksgiving, let us shout joyfully to Him with psalms. For the Lord is a great God and a great King above all gods."'},
+         {bonus: '1 Thessalonians 1:3\n"We ought always to give thanks to God for you brethren...your faith is greatly enlarged and the love of each one of you towards one another grows even greater."'}]},
+         {Think: [{sun: 'Psalm 145:18\nTRUTH\n"The Lord is near to all who call upon Him, to all who call upon Him in TRUTH."'}, 
+         {mon: 'Luke 8:15\nHONESTY\n"But the seed in the good soil, these are the ones who have heard the word in an HONEST and good heart, and hold it fast and bear fruit with perseverance."'},
+          {tue: 'Psalm 19:8\nRIGHT\n"The precepts of the Lord are RIGHT, rejoicing the heart."'}, 
+         {wed: '1 Timothy 1:5\nPURITY\n"But the goal of our instruction is love from a PURE heart and a good conscience and a sincere faith."'},
+          {thu: 'Isaiah 52:7\nLOVELY\n"How LOVELY on the mountains are the feet of him who brings good news, who announces peace and brings good news of happiness, and who announces salvation...your God reigns!"'}, 
+         {fri: 'John 12:36-38\nGOOD REPUTE\n"These things Jesus spoke,...But though He had performed so many signs before them, yet they were not believing in Him...Lord, who has believed our REPORT?"'},
+          {sat: '1 Peter 2:9\nEXCELLENCE\n"But you are a chosen race, a royal priesthood, a holy nation, a people for God\'s own possession so that you may proclaim the EXCELLENCE of Him."'},
+          {bonus: 'Psalm 103:1\nPRAISE\n"BLESS the Lord O my soul; and all that is within me, BLESS His Holy Name."'}]}];
+let dailyQuote;
 
 window.onload = function() {
     displayedDay.innerHTML = daysArray[day];
@@ -234,6 +316,8 @@ function seriesSelect() {
         // prevBtn.innerHTML = shortDaysArray[prevDay];
         // nextBtn.innerHTML = shortDaysArray[nextDay];
         updateScripture()
+        
+
     }
   
     else {
@@ -255,6 +339,7 @@ function seriesSelect() {
         // prevBtn.innerHTML = shortDaysArray[prevDay - 1];
         // nextBtn.innerHTML = shortDaysArray[nextDay + 1];
         updateScripture()
+        
     }
   }
 
@@ -275,6 +360,7 @@ function seriesSelect() {
         // prevBtn.innerHTML = shortDaysArray[prevDay];
         // nextBtn.innerHTML = shortDaysArray[nextDay];
         updateScripture()
+        
     }
    
     else {
@@ -296,6 +382,7 @@ function seriesSelect() {
         // prevBtn.innerHTML = shortDaysArray[prevDay - 1];
         // nextBtn.innerHTML = shortDaysArray[nextDay + 1];
         updateScripture()
+        
     }
 }
 
@@ -311,21 +398,7 @@ viewSelection.addEventListener('click', event => {
     //scriptureMatrix[1].Forgiveness[0].sun
     //scriptureMatrix[1].Forgiveness[1].mon
     updateScripture()//moved code below to the updateScripture function
-    // scriptureMatrix.forEach(script => {
-    //   //console.log(image)
-    //   if (script[selection]) {
-    //     console.log(script[selection]);
-    //      script[selection].forEach(daily => {
-    //        if (daily[shortDaysArray[currentDay]]) {
-    //          console.log(daily[shortDaysArray[currentDay]]);
-    //         scriptView.innerHTML = daily[shortDaysArray[currentDay]];
-    //        }
-
-    //      })
-        
-    //   }
-
-    // });
+    
 
   }
   
@@ -341,6 +414,7 @@ function updateScripture() {
        script[selection].forEach(daily => {
          if (daily[shortDaysArray[currentDay]]) {
            console.log(daily[shortDaysArray[currentDay]]);
+           dailyScript = daily[shortDaysArray[currentDay]];
           scriptView.innerHTML = daily[shortDaysArray[currentDay]];
          }
 
@@ -348,5 +422,54 @@ function updateScripture() {
       
     }
 
+  });
+}
+
+scriptView.addEventListener('click', event => {
+  event.preventDefault();
+  console.log("clicked on scriptView element", dailyScript);
+  viewChanger()
+  // if (scriptView.innerHTML === dailyScript) {
+  //   updateQuote()
+  // }
+  // if (scriptView.innerHTML === dailyQuote) {
+  //   console.log("the dailyquote = ",dailyQuote)
+  //   //scriptView.classList.toggle('showScripture');
+  //   console.log("the scriptView.innerHTML contains: ",scriptView.innerHTML);
+  //   updateScripture()
+  // }
+});
+
+function viewChanger() {
+  console.log("viewChanger function running. The scriptView.innerHTML = ", scriptView.innerHTML);
+  if (scriptView.innerHTML === dailyScript) {
+    dailyScript = "";
+    updateQuote()
+  }
+  else if (scriptView.innerHTML === dailyQuote) {
+    console.log("the dailyquote = ",dailyQuote)
+    //scriptView.classList.toggle('showScripture');
+    console.log("the scriptView.innerHTML contains: ",scriptView.innerHTML);
+    updateScripture()
+  }
+}
+
+function updateQuote() {
+  scriptView.innerHTML = "";
+  console.log("quote update function ran and the innerHTML contains: ", scriptView.innerHTML);
+  quoteMatrix.forEach(quote => {
+    //console.log(image)
+    if (quote[selection]) {
+      console.log("here is the quote[selection]: ", quote[selection]);
+       quote[selection].forEach(dayQuote => {
+         if (dayQuote[shortDaysArray[currentDay]]) {
+           console.log(dayQuote[shortDaysArray[currentDay]]);
+           dailyQuote = dayQuote[shortDaysArray[currentDay]];
+          scriptView.innerHTML = dayQuote[shortDaysArray[currentDay]];
+          console.log("the quote added: ", dayQuote[shortDaysArray[currentDay]], "the innerHTML contains: ", scriptView.innerHTML);
+         }
+       })
+    }
+    
   });
 }
