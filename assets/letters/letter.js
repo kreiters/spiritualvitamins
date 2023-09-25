c8c3bc1276902fb643f6052b2d82217517144f48
Kerry Kreiter <wonproudamerican@yahoo.com>
2023-09-24 19:04:24 -0500

    added grief

--- assets/letters/letter.js
+++ assets/letters/letter.js
@@ -1,7 +1,7 @@
 const jumpLinks = document.querySelectorAll("a[href^='#']");
 let verseSelection;
-let urlBeg = "https://www.biblegateway.com/passage/?search=";
-let urlEnd = "&version=NASB1995";
+let urlBeg = "https://www.biblegateway.com/passage/?search=";// https://www.bible.com/bible/116/; https://www.bible.com/bible/1/rev.7.KJV
+let urlEnd = "&version=NASB1995";// .NASB1995;
 let verse;
 
 jumpLinks.forEach(link => {//get the selected verse
@@ -20,6 +20,7 @@ function getVerse(verseSelection) {
     // console.log(selection);
     // window.open(urlBeg + selection + urlEnd);
     let result = verseSelection.replace(/\s/g, "");//removes whitespace
+    // console.log(result)
     // window.open(urlBeg + result + urlEnd);
     sessionStorage.setItem('verse', verseSelection);
     window.open("Verse.html", "_self");
